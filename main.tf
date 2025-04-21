5c4b6da506aa2f22726d96a38363e82e749dd513
Alexandr1246 <alex.sh1246@gmail.com>
2025-04-21 12:47:11 +0300

    work creating ec2 instance

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ubuntu" {
-  ami           = "ami-0907c2c90a7d5fb04"
-  instance_type = "t2.micro"
+  ami           = "ami-04542995864e26699"
+  instance_type = "t3.micro"
   key_name      = "terraform-key"
 }
